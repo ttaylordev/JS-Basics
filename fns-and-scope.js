2d30b8690a552a03cc1367b8cb0de0af2f234dd2
Tyson Taylor <mellowt@gmail.com>
2015-12-04 13:31:40 -0700

    30/40

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -4,73 +4,53 @@
 var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
-
   //Code Here
-var function isTyler(name) {
-  if (name !== "Tyler")
+function isTyler(namef) {
+  if (namef !== "Tyler"){
     return false;
   }
-//Next problem
-
-
-
-//Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
-var function getName() {
-  
+  return true;
 }
-
+//Next problem
+//Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
   //Code Here
-
+  function getName(promptName) {
+    promptedName = prompt("please enter your name");
+    return promptedName;
+  }
 
 //Next Problem
-
-
-
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
-
   //Code Here
-
+function welcome(promptedName) {
+  var name = getName();
+  alert("Welcome, " + name);
+}
 
 //Next problem
-
-
-
-
 //What is the difference between arguments and parameters?
-
   //Answer Here
-
+/* A Parameter is a placeholder for an argument to be passed to a function*/
 
 //Next problem
-
-
-
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
-
-
   //Answer Here
-
-
+  /* NaN, undefined, 0, false, null, "" (empty string) */
 
 //Next Problem
-
-
-
 //Create a function called myName that returns your name
 
   //Code Here
-
-
-
+function myNName() {
+  var name = "Tyson Taylor";
+  return name;
+}
 //Now save the function definition of myName into a new variable called newMyName
-
   //Code Here
-
+var newMyName = myName();
 //Now alert the result of invoking newMyName
-
-
-
+allert(newMyName);
 //Next problem
 
 
